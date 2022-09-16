b47f00b56064b3ebb8dcfc1a06e7000852b08016
Jonathan Allen <jallen@snow.edu>
2022-09-16 10:02:12 -0600

    bring over content from other demo app

---
+++ src/Components/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function About() {
+  return (
+    <>
+    <Link to="/">Home</Link>
+    <h1>About this app</h1>
+    <div>This is a demo of what you can do with stuff.</div>
+    </>
+  )
+}
